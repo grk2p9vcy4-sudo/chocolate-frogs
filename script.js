8c636ae65accd79aceae0fa8ae5419ad66289746
grk2p9vcy4-sudo <grk2p9vcy4@privaterelay.appleid.com>
2026-08-01 16:33:08 -0500

    Create character cards data structure
    Added an array of character objects with names, rarities, and quotes.

---
+++ script.js
@@ -0,0 +1,52 @@
+const cards = [
+  {
+    name: "Albus Dumbledore",
+    rarity: "Legendary",
+    quote: "Happiness can be found even in the darkest of times."
+  },
+  {
+    name: "Harry Potter",
+    rarity: "Legendary",
+    quote: "The Boy Who Lived."
+  },
+  {
+    name: "Draco Malfoy",
+    rarity: "Epic",
+    quote: "A proud Slytherin wizard."
+  },
+  {
+    name: "Severus Snape",
+    rarity: "Epic",
+    quote: "Always."
+  },
+  {
+    name: "Hermione Granger",
+    rarity: "Rare",
+    quote: "Books and cleverness."
+  },
+  {
+    name: "Luna Lovegood",
+    rarity: "Rare",
+    quote: "A curious and brilliant witch."
+  },
+  {
+    name: "Ron Weasley",
+    rarity: "Common",
+    quote: "Wicked!"
+  },
+  {
+    name: "Neville Longbottom",
+    rarity: "Common",
+    quote: "A brave Gryffindor."
+  },
+  {
+    name: "Minerva McGonagall",
+    rarity: "Legendary",
+    quote: "Transfiguration professor."
+  },
+  {
+    name: "Bellatrix Lestrange",
+    rarity: "Epic",
+    quote: "A powerful dark witch."
+  }
+];
