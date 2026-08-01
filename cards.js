55238870bbda373cef985473d482ec98d5f48887
grk2p9vcy4-sudo <grk2p9vcy4@privaterelay.appleid.com>
2026-08-01 15:54:50 -0500

    Create cards.js with character details
    Added a collection of character cards with names, rarities, and quotes.

---
+++ cards.js
@@ -0,0 +1,52 @@
+const cards = [
+  {
+    name: "Albus Dumbledore",
+    rarity: "Legendary",
+    quote: "Happiness can be found even in the darkest of times."
+  },
+  {
+    name: "Draco Malfoy",
+    rarity: "Epic",
+    quote: "I’m Draco Malfoy."
+  },
+  {
+    name: "Hermione Granger",
+    rarity: "Rare",
+    quote: "Books and cleverness."
+  },
+  {
+    name: "Harry Potter",
+    rarity: "Legendary",
+    quote: "The Boy Who Lived."
+  },
+  {
+    name: "Ron Weasley",
+    rarity: "Common",
+    quote: "Wicked!"
+  },
+  {
+    name: "Luna Lovegood",
+    rarity: "Rare",
+    quote: "Things we lose have a way of coming back."
+  },
+  {
+    name: "Severus Snape",
+    rarity: "Epic",
+    quote: "Always."
+  },
+  {
+    name: "Bellatrix Lestrange",
+    rarity: "Epic",
+    quote: "A loyal follower of the Dark Lord."
+  },
+  {
+    name: "Sirius Black",
+    rarity: "Rare",
+    quote: "The ones that love us never really leave us."
+  },
+  {
+    name: "Minerva McGonagall",
+    rarity: "Legendary",
+    quote: "Transfiguration professor."
+  }
+];
